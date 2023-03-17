f389e8ce1ee86b0985e4ce6bd8829b53011560e1
TonyDolenti <antoniobdolenti@gmail.com>
2023-03-17 12:50:52 -0400

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string str)
+        {
+            str = str.ToLower();
+            var reversed = "";
+
+            if (str == null || str.Length == 0)
+            {
+                return false;
+            }
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reversed += str[i];
+            }
+            
+            if (reversed == str)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,32 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("dumbass", false)]
+        [InlineData("civic", true)]
+        [InlineData("happy", false)]
+        [InlineData("radar", true)]
+        [InlineData("specialist", false)]
+        [InlineData("tenet", true)]
+        [InlineData("", false)]
+        public void IsAPalindrome_Test(string str, bool expected)
         {
-
+            //Arrange
+            var test = new WordSmith();
+            //Act
+            var actual = test.IsAPalindrome(str);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
